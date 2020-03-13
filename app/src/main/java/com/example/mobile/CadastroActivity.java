66844265acccd431a49a630069a027685031a82f
lucazpinheiro <lucascpinheiro10@gmail.com>
2020-03-13 20:12:22 -0300

    fix ALL errors, no need to check

--- app/src/main/java/com/example/mobile/CadastroActivity.java
+++ app/src/main/java/com/example/mobile/CadastroActivity.java
@@ -11,32 +11,33 @@ import android.widget.Toast;
 
 public class CadastroActivity extends AppCompatActivity {
 
-    private EditText nome;
-    private EditText endereco;
+    public static final String REGISTRO_SALVO = "Registro salvo!";
+    private EditText name;
+    private EditText address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
-        nome = findViewById(R.id.formulario_nome);
-        endereco = findViewById(R.id.formulario_endereco);
+        name = findViewById(R.id.formName);
+        address = findViewById(R.id.formAddress);
 
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+        Button botaoSalvar = (Button) findViewById(R.id.formSave);
         botaoSalvar.setOnClickListener(new View.OnClickListener()  {
             @Override
             public void onClick (View v) {
-                String enderecoValue = endereco.getText().toString();
-                String nomeValue = nome.getText().toString();
+                String enderecoValue = address.getText().toString();
+                String nomeValue = name.getText().toString();
 
                 Intent intent = new Intent();
 
-                intent.putExtra("nome", nomeValue);
-                intent.putExtra("endereco", enderecoValue );
+                intent.putExtra("name", nomeValue);
+                intent.putExtra("address", enderecoValue );
 
                 setResult(MainActivity.CADATRADO_SUCESSO, intent);
 
-                Toast.makeText(CadastroActivity.this, "Registro salvo!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(CadastroActivity.this, REGISTRO_SALVO, Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
